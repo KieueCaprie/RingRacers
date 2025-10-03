7e8141915009bf8fbae797c107f25f75206fa46c
KieueCaprie <kiuufaun@proton.me>
2025-10-03 18:24:32 +0100

    turns out if you have losing set to true and retry set to false, you're treated as though you got a game over, whoops.

--- src/g_game.c
+++ src/g_game.c
@@ -3344,7 +3344,7 @@ void G_BeginLevelExit(void)
 					if (grandprixinfo.masterbots == false)
 					{
 						g_difficulty = 1;
-						continue;
+						g_exit.losing = false;
 					}
 					else
 					{
@@ -3357,8 +3357,8 @@ void G_BeginLevelExit(void)
 				if (G_GametypeUsesLives() && players[i].lives <= 0)
 					continue;
 				if (G_GametypeUsesLives() && players[i].lives >= 1 && g_difficulty == 2)
-				g_exit.retry = true;
-				break;
+					g_exit.retry = true;
+					break;
 			}
 		}
 	}
